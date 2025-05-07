b08b77078997833a96854bdbd0cb3a2568683c49
justmariaam <mhdez914@gmail.com>
2025-05-07 16:53:58 -0600

    07-formularios

---
+++ html00/formulario.js
@@ -0,0 +1,25 @@
+console.log("inicio")
+
+function muestra(){
+    let user= document.getElementById("user")
+    let password= document.getElementById("password")
+
+    console.log(user.value)
+    console.log(password.value)
+
+}
+//ejemplo de funcion de callback
+const boton = document.getElementById("boton")
+//boton.addEventListener("click" , muestra)
+
+//ejemplo de funcion de funció anonima de tipo arrow
+boton.addEventListener("click" , 
+    () => {
+        let user= document.getElementById("user")
+        let password= document.getElementById("password")
+ 
+        console.log(user.value)
+        console.log(password.value)
+
+    }
+)
